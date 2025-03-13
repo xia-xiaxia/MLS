515d2edc30a968bacf9f3d344296197b9667912e
Cypressclock <3185917002@qq.com>
2025-03-13 15:23:06 +0800

    Add files via upload

---
+++ Scripts/GameDataManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 队友的数据存储系统
+public class GameDataManager : MonoBehaviour
+{
+    public static GameDataManager Instance;
+
+    public HashSet<string> fullPartners = new HashSet<string>();
+    public int totalPartnerFragments = 0;
+    public Dictionary<string, Rarity> recipeMaxRarity = new Dictionary<string, Rarity>();
+    public int totalRecipeExperience = 0;
+    public int totalIngredientExperience = 0;
+    public int totalGold;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void UpdateRecipeMaxRarity(string recipeName, Rarity rarity)
+    {
+        if (!recipeMaxRarity.ContainsKey(recipeName) || rarity > recipeMaxRarity[recipeName])
+        {
+            recipeMaxRarity[recipeName] = rarity;
+        }
+    }
+
+    public void AddIngredientExperience(int experience) => totalIngredientExperience += experience;
+    public void AddRecipeExperience(int experience) => totalRecipeExperience += experience;
+    public void AddPartnerFragments(int fragments) => totalPartnerFragments += fragments;
+    public void SaveFullPartner(string partnerName) => fullPartners.Add(partnerName);
+}
