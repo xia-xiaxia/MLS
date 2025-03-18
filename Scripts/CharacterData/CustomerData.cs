5451640915bf202f58bd0dea698445d8ce062de8
Cypressclock <3185917002@qq.com>
2025-03-18 21:19:40 +0800

    Update and rename CustomerData.cs to CustomerSystem.cs

--- Scripts/CharacterData/CustomerData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Data/Customer")]
-public class CustomerData : ScriptableObject
-{
-    public float walkingSpeed=4f;        // 基础采购成本
-    public float eatingSpeed=3f;       // 销售价格
-  
-}
